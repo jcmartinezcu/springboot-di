e30d03be36f968ea97a74333053f833573636375
jcmartinez <jcmartinezcu@gmail.com>
2024-06-07 20:06:43 +0200

    anotaciones @Autowire y @Component

--- src/main/java/com/jc/springboot/di/app/springbootdi/services/ProductServiceImpl.java
+++ src/main/java/com/jc/springboot/di/app/springbootdi/services/ProductServiceImpl.java
@@ -3,12 +3,17 @@ package com.jc.springboot.di.app.springbootdi.services;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import com.jc.springboot.di.app.springbootdi.models.Product;
 import com.jc.springboot.di.app.springbootdi.repositories.ProductRepositoryImpl;
 
+@Component
 public class ProductServiceImpl implements ProductService {
 
-    private ProductRepositoryImpl repository = new ProductRepositoryImpl();
+    @Autowired
+    private ProductRepositoryImpl repository;
 
    @Override
     public List<Product> findAll(){
