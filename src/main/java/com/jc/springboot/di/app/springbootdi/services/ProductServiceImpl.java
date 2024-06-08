f64f3d90fc280c86a3268d3ea96e8adddb552f90
jcmartinez <jcmartinezcu@gmail.com>
2024-06-08 19:20:38 +0200

    leer desde el properties

--- src/main/java/com/jc/springboot/di/app/springbootdi/services/ProductServiceImpl.java
+++ src/main/java/com/jc/springboot/di/app/springbootdi/services/ProductServiceImpl.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 import com.jc.springboot.di.app.springbootdi.models.Product;
@@ -12,36 +14,38 @@ import com.jc.springboot.di.app.springbootdi.repositories.ProductRepository;
 @Service
 public class ProductServiceImpl implements ProductService {
 
-  
-    private ProductRepository repository;
 
    
-   public ProductServiceImpl(@Qualifier("productList")ProductRepository repository) {
+
+    @Value("${config.price.tax}")
+    private Double tax;
+    private ProductRepository repository;
+
+    public ProductServiceImpl(@Qualifier("productList") ProductRepository repository) {
         this.repository = repository;
     }
 
-@Override
-    public List<Product> findAll(){
+    @Override
+    public List<Product> findAll() {
 
         return repository.findAll().stream()
-                            .map(p->{
-                                Double priceTax = p.getPrice() * 1.25d;
-                                // Product newProd = new Product(p.getId(), p.getName(), priceTax.longValue());                                
-                                Product newProd =  (Product) p.clone();
-                                newProd.setPrice(priceTax.longValue());   
-                                return newProd;                             
-                                // p.setPrice(priceTax.longValue());
-                                // return p;
-                            })
-                            .collect(Collectors.toList());
+                .map(p -> {
+                    // Double priceTax = p.getPrice() * environment.getProperty("config.price.tax", Double.class);
+                    Double priceTax = p.getPrice() * tax;
+                    // Product newProd = new Product(p.getId(), p.getName(), priceTax.longValue());
+                    Product newProd = (Product) p.clone();
+                    newProd.setPrice(priceTax.longValue());
+                    return newProd;
+                    // p.setPrice(priceTax.longValue());
+                    // return p;
+                })
+                .collect(Collectors.toList());
     }
 
     @Override
-    public Product findById(Long id){
+    public Product findById(Long id) {
 
         return repository.findById(id);
     }
-
-    
 
 }
