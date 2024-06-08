775b06ac012a86c7e96ec1859e5715b06a3c477e
jcmartinez <jcmartinezcu@gmail.com>
2024-06-08 15:23:44 +0200

    anotación @Primary

--- src/main/java/com/jc/springboot/di/app/springbootdi/repositories/ProductRepositoryImpl.java
+++ src/main/java/com/jc/springboot/di/app/springbootdi/repositories/ProductRepositoryImpl.java
@@ -3,10 +3,12 @@ package com.jc.springboot.di.app.springbootdi.repositories;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Repository;
 
 import com.jc.springboot.di.app.springbootdi.models.Product;
 
+@Primary
 @Repository
 public class ProductRepositoryImpl implements ProductRepository {
 
