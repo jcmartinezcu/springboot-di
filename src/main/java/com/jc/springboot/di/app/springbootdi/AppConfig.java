85d0a333909c7fe7035aba146af068b130d6b912
jcmartinez <jcmartinezcu@gmail.com>
2024-06-08 20:12:19 +0200

    Variantes en el componente registrado con @configuration y @Bean

--- src/main/java/com/jc/springboot/di/app/springbootdi/AppConfig.java
+++ src/main/java/com/jc/springboot/di/app/springbootdi/AppConfig.java
@@ -1,8 +1,10 @@
 package com.jc.springboot.di.app.springbootdi;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.io.Resource;
 
 import com.jc.springboot.di.app.springbootdi.repositories.ProductRepository;
 import com.jc.springboot.di.app.springbootdi.repositories.ProductRepositoryJson;
@@ -11,8 +13,11 @@ import com.jc.springboot.di.app.springbootdi.repositories.ProductRepositoryJson;
 @PropertySource("classpath:config.properties")
 public class AppConfig {
 
+    @Value("classpath:json/product.json")
+    private Resource resource;
+
     @Bean
     ProductRepository productRepositoryJson(){
-        return new ProductRepositoryJson();
+        return new ProductRepositoryJson(resource);
     }
 }
--- src/main/java/com/jc/springboot/di/app/springbootdi/repositories/ProductRepositoryJson.java
+++ src/main/java/com/jc/springboot/di/app/springbootdi/repositories/ProductRepositoryJson.java
@@ -7,25 +7,28 @@ import java.util.List;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 
-import com.fasterxml.jackson.core.exc.StreamReadException;
-import com.fasterxml.jackson.databind.DatabindException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jc.springboot.di.app.springbootdi.models.Product;
 
 public class ProductRepositoryJson implements ProductRepository {
 
     private List<Product> list;
-    
+
+
 
     public ProductRepositoryJson() {
         Resource resource = new ClassPathResource("json/product.json");
+        readValueJson(resource);
+    }
+    public ProductRepositoryJson(Resource resource) {
+        // Resource resource = new ClassPathResource("json/product.json");
+        readValueJson(resource);
+    }
+    
+    private void readValueJson(Resource resource) {
         ObjectMapper objectMapper = new ObjectMapper();
         try {
-            list = Arrays.asList(objectMapper.readValue(resource.getFile(), Product[].class));
-        } catch (StreamReadException e) {
-            e.printStackTrace();
-        } catch (DatabindException e) {
-            e.printStackTrace();
+            list = Arrays.asList(objectMapper.readValue(resource.getInputStream(), Product[].class));
         } catch (IOException e) {
             e.printStackTrace();
         }
